e35ad47f4b630bddee8c79984aad27d6a07b2374
Nikita Kuklev <spontaneouscoder@gmail.com>
2022-03-03 17:55:47 -0800

    import sddsfile at root level

--- pysdds/__init__.py
+++ pysdds/__init__.py
@@ -2,6 +2,7 @@ __author__ = "Nikita Kuklev"
 
 from .readers import read
 from .writers import write
+from .structures import SDDSFile
 
 from . import _version
 __version__ = _version.get_versions()['version']
