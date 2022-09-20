8ede5b1ac9ed90a24cc5a80be385509bbcb86763
nikitakuklev <spontaneouscoder@gmail.com>
2022-09-19 19:20:26 -0500

    Remove relative imports

--- pysdds/__init__.py
+++ pysdds/__init__.py
@@ -1,8 +1,8 @@
 __author__ = "Nikita Kuklev"
 
-from .readers import read
-from .writers import write
-from .structures import SDDSFile
+from pysdds.readers import read
+from pysdds.writers import write
+from pysdds.structures import SDDSFile
 
 from . import _version
 __version__ = _version.get_versions()['version']
