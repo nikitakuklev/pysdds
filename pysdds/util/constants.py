58a46d347429443565e50faf71f8a0516d3b7fe0
nikitakuklev <spontaneouscoder@gmail.com>
2022-10-03 20:30:25 -0500

    Add more supported data types up to SDDSv5

--- pysdds/util/constants.py
+++ pysdds/util/constants.py
@@ -1,35 +1,75 @@
 import numpy as np
 
 # SDDS specification has short, long, float, double, character, or string
-_NUMPY_DTYPES = {'short': 'i2', 'ushort': 'u2', 'long': 'i4', 'float': 'f4', 'double': 'f8',
+# SDDS 2 adds ulong, ushort
+# SDDS 4/5 adds long64, ulong64
+# Unclear which version adds longdouble - handling this type is very problematic because it changes size based on
+# the OS and 32/64 version to ensure byte alignment. Namely, Windows has np.longdouble==np.double, x86 unix = np.float96, x64 unix = np.float128, PowerPC handling is compiler-dependent
+_NUMPY_DTYPES = {'short': 'i2', 'ushort': 'u2',
+                 'long': 'i4', 'ulong': 'u4',
+                 'long64': 'i8', 'ulong64': 'u8',
+                 'float': 'f4', 'double': 'f8',
+                 'longdouble': 'g',
                  'character': object, 'string': object}
 
-# Need to look into i8 support
-_NUMPY_DTYPES_INV = {np.dtype(np.int16): 'short', np.dtype(np.int32): 'long', np.dtype(np.int64): 'long',
+_NUMPY_DTYPES_INV = {np.dtype(np.int16): 'short', np.dtype(np.uint16): 'ushort',
+                     np.dtype(np.int32): 'long', np.dtype(np.int32): 'ulong',
+                     np.dtype(np.int64): 'long64', np.dtype(np.int64): 'ulong64',
                      np.dtype(np.float64): 'double', np.dtype(np.float32): 'float',
+                     np.dtype(np.longdouble): 'longdouble',
                      object: 'string', np.dtype('O'): 'string'}
 
 # On all 'reasonable' architectures, things will be little endian, but plenty of old files floating around
-_NUMPY_DTYPE_LE = {'short': np.dtype('<i2'), 'ushort': np.dtype('<u2'), 'long': np.dtype('<i4'),
-                   'float': np.dtype('<f4'), 'double': np.dtype('<f8'), 'character': np.dtype('<i1'),
-                   'string': object}
-_NUMPY_DTYPE_BE = {'short': np.dtype('>i2'), 'ushort': np.dtype('>u2'), 'long': np.dtype('>i4'),
-                   'float': np.dtype('>f4'), 'double': np.dtype('>f8'), 'character': np.dtype('>i1'),
-                   'string': object}
-_NUMPY_DTYPE_FINAL = {'short': np.dtype('i2'), 'ushort': np.dtype('u2'), 'long': np.dtype('i4'),
-                      'float': np.dtype('f4'), 'double': np.dtype('f8'), 'character': object,
-                      'string': object}
-_PYTHON_TYPE_FINAL = {'short': np.dtype('i2'), 'ushort': np.dtype('u2'), 'long': np.dtype('i4'),
-                      'float': np.dtype('f4'), 'double': np.dtype('f8'), 'character': str,
-                      'string': str}
-_PYTHON_TYPE_INV = {int: 'long', float: 'double', str: 'string'}
-_STRUCT_STRINGS_LE = {'short': '<h', 'ushort': '<H', 'long': '<l',
-                      'float': '<f', 'double': '<d', 'character': '<c', 'string': object}
-_STRUCT_STRINGS_BE = {'short': '>h', 'ushort': '>H', 'long': '>l',
-                      'float': '>f', 'double': '>d', 'character': '>c', 'string': object}
+_NUMPY_DTYPE_LE = {'short': np.dtype('<i2'), 'ushort': np.dtype('<u2'),
+                   'long': np.dtype('<i4'), 'ulong': np.dtype('<u4'),
+                   'long64': np.dtype('<i8'), 'ulong64': np.dtype('<u8'),
+                   'float': np.dtype('<f4'), 'double': np.dtype('<f8'),
+                   'longdouble': np.dtype('<g', align=True),
+                   'character': np.dtype('<i1'), 'string': object}
 
-# Expected field lengths in bytes for 32bit architecture (which SDDS was made for initially)
-_NUMPY_DTYPE_SIZES = {'short': 2, 'ushort': 2, 'long': 4, 'float': 4, 'double': 8, 'character': 1, 'string': None}
+_NUMPY_DTYPE_BE = {'short': np.dtype('>i2'), 'ushort': np.dtype('>u2'),
+                   'long': np.dtype('>i4'), 'ulong': np.dtype('>u4'),
+                   'long64': np.dtype('>i8'), 'ulong64': np.dtype('>u8'),
+                   'float': np.dtype('>f4'), 'double': np.dtype('>f8'),
+                   'longdouble': np.dtype('>g'),
+                   'character': np.dtype('<i1'), 'string': object}
+
+_NUMPY_DTYPE_FINAL = {'short': np.dtype('i2'), 'ushort': np.dtype('u2'),
+                      'long': np.dtype('i4'), 'ulong': np.dtype('u4'),
+                      'long64': np.dtype('i8'), 'ulong64': np.dtype('u8'),
+                      'float': np.dtype('f4'), 'double': np.dtype('f8'),
+                      'longdouble': np.dtype('g'),
+                      'character': object, 'string': object}
+
+_PYTHON_TYPE_FINAL = {'short': np.dtype('i2'), 'ushort': np.dtype('u2'),
+                      'long': np.dtype('i4'), 'ulong': np.dtype('u4'),
+                      'long64': np.dtype('i8'), 'ulong64': np.dtype('u8'),
+                      'float': np.dtype('f4'), 'double': np.dtype('f8'),
+                      'longdouble': np.dtype('g'),
+                      'character': str, 'string': str}
+
+_PYTHON_TYPE_INV = {int: 'long', float: 'double', str: 'string'}
+_STRUCT_STRINGS_LE = {'short': '<h', 'ushort': '<H',
+                      'long': '<l', 'ulong': '<L',
+                      'long64': '<q', 'ulong64': '<Q',
+                      'float': '<f', 'double': '<d',
+                      'longdouble': '16s',
+                      'character': '<c', 'string': object}
+
+_STRUCT_STRINGS_BE = {'short': '>h', 'ushort': '>H',
+                      'long': '>l', 'ulong': '>L',
+                      'long64': '>q', 'ulong64': '>Q',
+                      'float': '>f', 'double': '>d',
+                      'longdouble': '16s',
+                      'character': '>c', 'string': object}
+
+# Expected field lengths in bytes based on what sddsconvert outputs on x86-64
+_NUMPY_DTYPE_SIZES = {'short': 2, 'ushort': 2,
+                      'long': 4, 'ulong': 4,
+                      'long64': 8, 'ulong64': 8,
+                      'float': 4, 'double': 8,
+                      'longdouble': 16,
+                      'character': 1, 'string': None}
 
 # Expected keys for various SDDS namelists
 _KEYS_DESCRIPTION = {'text', 'contents'}
