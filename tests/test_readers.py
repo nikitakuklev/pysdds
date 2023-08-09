5c2526c2c79761dd3105d827a060f6f83eeaa83d
nikitakuklev <spontaneouscoder@gmail.com>
2023-08-09 15:55:58 -0500

    Refactor read tests

--- tests/test_readers.py
+++ tests/test_readers.py
@@ -1,3 +1,5 @@
+import io
+
 import pytest
 import pysdds
 from pathlib import Path
@@ -7,125 +9,112 @@ cwd = Path(__file__).parent
 root_sources = cwd / 'files'
 print(f'Executing readers in {cwd=} {root_sources=}')
 subroots = ['sources', 'sources_binary_rowmajor', 'sources_binary_colmajor', 'sources_ascii']
-folders = {sub: root_sources / sub for sub in subroots}
-files = {sub: list(d.glob('*')) for sub, d in folders.items()}
-
 to_str = lambda l: [str(s) for s in l]
 
-files_sources = to_str((root_sources / 'sources').glob('*'))
-files_ascii = to_str((root_sources / 'sources_ascii').glob('*'))
-files_binary_colmajor = to_str((root_sources / 'sources_binary_colmajor').glob('*'))
-files_binary_rowmajor = to_str((root_sources / 'sources_binary_rowmajor').glob('*'))
-files_compressed = to_str((root_sources / 'sources_compressed').glob('*'))
-files_large = to_str((root_sources / 'sources_large').glob('*'))
+ff = to_str((root_sources / 'sources').glob('*'))
+ff_ascii = to_str((root_sources / 'sources_ascii').glob('*'))
+ff_binary_colmajor = to_str((root_sources / 'sources_binary_colmajor').glob('*'))
+ff_binary_rowmajor = to_str((root_sources / 'sources_binary_rowmajor').glob('*'))
 
-all_files_sources = files_sources + files_ascii + files_binary_colmajor + files_binary_rowmajor
-all_files_sources_large = to_str(root_sources.glob('sources_large*/*'))
-all_files_compressed = to_str(root_sources.glob('sources_compressed*/*'))
+fc = to_str((root_sources / 'sources_compressed').glob('*'))
+fc_ascii = to_str((root_sources / 'sources_compressed_ascii').glob('*'))
+fc_binary_colmajor = to_str((root_sources / 'sources_compressed_binary_colmajor').glob('*'))
+fc_binary_rowmajor = to_str((root_sources / 'sources_compressed_binary_rowmajor').glob('*'))
 
-all_files = (files_sources + files_ascii + files_binary_colmajor + files_binary_rowmajor +
-             all_files_sources_large + all_files_compressed)
+fl = to_str((root_sources / 'sources_large').glob('*'))
+fl_ascii = to_str((root_sources / 'sources_large_ascii').glob('*'))
+fl_binary_colmajor = to_str((root_sources / 'sources_large_binary_colmajor').glob('*'))
+fl_binary_rowmajor = to_str((root_sources / 'sources_large_binary_rowmajor').glob('*'))
+
+files_all_nolarge = (ff + ff_ascii + ff_binary_colmajor
+                     + ff_binary_rowmajor + fc)
+files_all = (ff + ff_ascii + ff_binary_colmajor + ff_binary_rowmajor +
+             fl + fc)
 
 get_names = lambda xa: [Path(x).name for x in xa]
 get_name = lambda x: Path(x).name
 
 # Generate pairs [(file1, file1_ascii, ...), (file2, ...)] for equality testing
-set_sources = set(get_names(files_sources))
-set_ascii_rowmajor = set(get_names(files_ascii))
-set_binary_rowmajor = set(get_names(files_binary_rowmajor))
-set_binary_colmajor = set(get_names(files_binary_colmajor))
+file_name_dict = {}
+for l in [ff + fc + fl, ff_ascii + fc_ascii + fl_ascii,
+          ff_binary_rowmajor + fc_binary_rowmajor + fl_binary_rowmajor,
+          ff_binary_colmajor + fc_binary_colmajor + fl_binary_colmajor]:
+    for f in l:
+        name = get_name(f)
+        if name in file_name_dict:
+            file_name_dict[name].append(f)
+        else:
+            file_name_dict[name] = [f]
 
-set_union = set_sources.intersection(set_ascii_rowmajor, set_binary_rowmajor, set_binary_colmajor)
-sets_list = []
-for f in set_sources:
-    if f in set_union:
-        sets_list.append([str(root_sources / ('sources') / f),
-                          str(root_sources / ('sources_binary_colmajor') / f),
-                          str(root_sources / ('sources_binary_rowmajor') / f),
-                          str(root_sources / ('sources_ascii') / f)])
-    else:
-        continue
+all_tuples = list(file_name_dict.values())
 
-# This will create AB and BA comparison to ensure things are commutative
-all_tuples = [x for files in sets_list for x in itertools.product(files, repeat=2)]
-all_files1 = [x[0] for x in all_tuples]
-all_files2 = [x[1] for x in all_tuples]
+file_name_dict = {}
+for l in [ff_binary_rowmajor, ff_binary_colmajor]:
+    for f in l:
+        name = get_name(f)
+        if name in file_name_dict:
+            file_name_dict[name].append(f)
+        else:
+            file_name_dict[name] = [f]
+
+binary_tuples = list(file_name_dict.values())
 
 
-@pytest.mark.parametrize("file_root", files_sources)
+@pytest.mark.parametrize("file_root", files_all)
 def test_read_header(file_root):
     pysdds.read(file_root, header_only=True)
 
 
-@pytest.mark.parametrize("file_root", files_sources)
+@pytest.mark.parametrize("file_root", ff + fc)
 def test_read(file_root):
     sdds = pysdds.read(file_root)
     sdds.validate_data()
 
 
-@pytest.mark.parametrize("file_root", files_binary_colmajor)
-def test_read_binary1(file_root):
-    sdds = pysdds.read(file_root)
-    sdds.validate_data()
+# TODO: compressed stream support
+@pytest.mark.parametrize("file_root", ff)
+def test_read_buffer(file_root):
+    with open(file_root, 'rb') as fs:
+        buf = fs.read()
+        stream = io.BytesIO(buf)
+        bstream = io.BufferedReader(stream)
+        sdds = pysdds.read(bstream)
+        sdds.validate_data()
 
 
-@pytest.mark.parametrize("file_root", files_binary_rowmajor)
-def test_read_binary2(file_root):
-    sdds = pysdds.read(file_root)
-    sdds.validate_data()
-
-
-@pytest.mark.parametrize("file_root", files_ascii)
-def test_read_ascii(file_root):
-    sdds = pysdds.read(file_root)
-    sdds.validate_data()
-
-
-@pytest.mark.parametrize("file_root", all_files)
-def test_read_all(file_root):
-    sdds = pysdds.read(file_root)
-    sdds.validate_data()
-
-
-@pytest.mark.parametrize("file_root", files_compressed)
-def test_read_compressed(file_root):
-    sdds = pysdds.read(file_root)
-    sdds.validate_data()
-
-
-@pytest.mark.parametrize("file_root", files_large)
+@pytest.mark.parametrize("file_root", fl)
 def test_read_large(file_root):
     sdds = pysdds.read(file_root)
     sdds.validate_data()
 
 
-file_ts = [str(root_sources / 'sources_compressed/timeSeries.sdds.xz')]
-
-
-@pytest.mark.parametrize("file_root", file_ts)
-def test_page_mask(file_root):
+@pytest.mark.parametrize("file_root", ff_binary_colmajor + fc_binary_colmajor)
+def test_read_binary1(file_root):
     sdds = pysdds.read(file_root)
-    assert sdds.n_pages == 157
-    sdds.columns_to_df(156)
-    with pytest.raises(ValueError):
-        sdds.columns_to_df(157)
-    with pytest.raises(ValueError):
-        sdds.columns_to_df(-1)
-
-    sdds = pysdds.read(file_root, pages=[0])
-    sdds.columns_to_df(0)
-    assert sdds.n_pages == 1
-
-    sdds = pysdds.read(file_root, pages=[0, 5])
-    assert sdds.n_pages == 2
-    sdds.columns_to_df(0)
-    sdds.columns_to_df(1)
-    with pytest.raises(ValueError):
-        sdds.columns_to_df(2)
+    sdds.validate_data()
 
 
-@pytest.mark.parametrize("file1, file2", all_tuples)
-def test_read_data_compare(file1, file2):
-    sdds1 = pysdds.read(file1)
-    sdds2 = pysdds.read(file2)
-    assert sdds1.compare(sdds2, eps=1e-5, raise_error=True, fixed_value_equivalent=True)
+@pytest.mark.parametrize("file_root", ff_binary_rowmajor + fc_binary_rowmajor)
+def test_read_binary2(file_root):
+    sdds = pysdds.read(file_root)
+    sdds.validate_data()
+
+
+@pytest.mark.parametrize("file_root", ff_ascii + fc_ascii)
+def test_read_ascii(file_root):
+    sdds = pysdds.read(file_root)
+    sdds.validate_data()
+
+
+@pytest.mark.parametrize("files", binary_tuples)
+def test_read_data_compare_exact(files):
+    sdds_objects = [pysdds.read(f) for f in files]
+    for pair in itertools.product(sdds_objects, repeat=2):
+        assert pair[0].compare(pair[1], raise_error=True, fixed_value_equivalent=True)
+
+
+@pytest.mark.parametrize("files", all_tuples)
+def test_read_data_compare_all(files):
+    sdds_objects = [pysdds.read(f) for f in files]
+    for pair in itertools.product(sdds_objects, repeat=2):
+        assert pair[0].compare(pair[1], eps=1e-5, raise_error=True, fixed_value_equivalent=True)
--- tests/test_readers_indexing.py
+++ tests/test_readers_indexing.py
@@ -1,18 +1,20 @@
+import itertools
+import random
+
 import numpy as np
 import pytest
 import pysdds
 from pathlib import Path
+import random
 
 cwd = Path(__file__).parent
 root_sources = cwd / 'files'
 to_str = lambda l: [str(s) for s in l]
-files_ascii = to_str((root_sources / 'sources_ascii').glob('*'))
-get_names = lambda xa: [Path(x).name for x in xa]
-get_name = lambda x: Path(x).name
+ff = to_str((root_sources / 'sources').glob('*'))
 
 
-@pytest.mark.parametrize("file_root", files_ascii)
-def test_read_ascii(file_root):
+@pytest.mark.parametrize("file_root", ff)
+def test_read_cols(file_root):
     sdds = pysdds.read(file_root)
     sdds.validate_data()
     for c in sdds.columns:
@@ -21,6 +23,16 @@ def test_read_ascii(file_root):
             a = col[p]
             b = col.data[p]
             assert np.array_equal(a, b)
+
+    pairs = list(itertools.product(sdds.column_names, repeat=2))
+    for random_cols in random.sample(pairs, min(len(pairs), 10)):
+        sdds2 = pysdds.read(file_root, cols=random_cols)
+        for c in sdds2.columns:
+            if c._enabled:
+                c2 = sdds.column_dict[c.name]
+                c.compare(c2, raise_error=True)
+            else:
+                assert len(c.data) == 0
 
 
 file_ts = [str(root_sources / 'sources_compressed/timeSeries.sdds.xz')]
@@ -46,3 +58,21 @@ def test_page_mask(file_root):
     sdds.columns_to_df(1)
     with pytest.raises(ValueError):
         sdds.columns_to_df(2)
+
+
+@pytest.mark.parametrize("file_root", file_ts)
+def test_col_mask(file_root):
+    sdds = pysdds.read(file_root, cols=['ReadbackName'])
+    sdds.validate_data()
+    assert sdds.n_pages == 157
+    assert sdds.n_columns == 2
+    assert len([c for c in sdds.columns if c._enabled]) == 1
+    assert sdds.n_parameters == 11
+
+    sdds = pysdds.read(file_root, cols=[1])
+    assert sdds.n_pages == 157
+    assert sdds.n_columns == 2
+    assert not sdds.columns[0]._enabled
+    assert sdds.columns[1]._enabled
+    assert sdds.n_parameters == 11
+    sdds.validate_data()
