8dba73fd4eef63efc3efa046e8c21cf73369d5c9
nikitakuklev <spontaneouscoder@gmail.com>
2023-09-11 12:41:31 -0500

    Start io rework

---
+++ pysdds/util/io.py
@@ -0,0 +1,14 @@
+from io import BufferedReader, RawIOBase
+
+
+class SizedBufferedReader(BufferedReader):
+
+    def __init__(self, raw: RawIOBase, size):
+        super().__init__(raw)
+        self.size = size
+
+    def __len__(self):
+        return self.size
+
+
+
