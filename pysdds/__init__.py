7cfa73801ccbe00dcce00e3423c84e18b3a8a188
nikitakuklev <spontaneouscoder@gmail.com>
2025-05-21 11:52:25 -0500

    Cleanup

--- pysdds/__init__.py
+++ pysdds/__init__.py
@@ -3,7 +3,3 @@ __author__ = "Nikita Kuklev"
 from pysdds.readers import read  # noqa: F401
 from pysdds.writers import write  # noqa: F401
 from pysdds.structures import SDDSFile  # noqa: F401
-
-from . import _version
-
-__version__ = _version.get_versions()["version"]
