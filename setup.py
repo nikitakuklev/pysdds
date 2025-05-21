bd8dc35442e2239de7eb851e1d4b5ff5d2e8cd8c
nikitakuklev <spontaneouscoder@gmail.com>
2025-05-21 11:39:10 -0500

    Move to pyproject build

--- setup.py
+++ setup.py
@@ -1,47 +1,11 @@
-import setuptools
-import versioneer
+from setuptools import setup
+try:
+    import versioneer
+except ImportError:
+    import sys
+    import os
+    sys.path.insert(0, os.path.dirname(__file__))
+    import versioneer
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    LONG_DESCRIPTION = fh.read()
-
-INSTALL_REQUIRES = [
-    'numpy>=1.13.3',
-    'pandas>=1.0'
-]
-EXTRAS_REQUIRES = {
-    "develop": [
-        "pytest>=6.0",
-        "coverage",
-        "pytest-cov"
-    ]
-}
-LICENSE = 'MIT'
-DESCRIPTION = 'Package to read and write SDDS files'
-CLASSIFIERS = [
-    "Programming Language :: Python :: 3.8",
-    "Programming Language :: Python :: 3.9",
-    "Programming Language :: Python :: 3.10",
-    "Programming Language :: Python :: 3.11",
-    "Programming Language :: Python :: 3.12",
-    "License :: OSI Approved :: MIT License",
-    "Operating System :: OS Independent",
-    "Topic :: Scientific/Engineering",
-    "Intended Audience :: Science/Research",
-]
-
-setuptools.setup(
-    name="pysdds",
-    author="Nikita Kuklev",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    packages=setuptools.find_packages(include=['pysdds*']),
-    description=DESCRIPTION,
-    license=LICENSE,
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    classifiers=CLASSIFIERS,
-    platforms="any",
-    install_requires=INSTALL_REQUIRES,
-    python_requires=">=3.8",
-    extras_require=EXTRAS_REQUIRES
-)
+setup(version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass())
