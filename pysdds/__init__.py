53fdd23697d40a733f865f653e13adf37d89fb4c
nikitakuklev <spontaneouscoder@gmail.com>
2025-05-21 11:58:31 -0500

    Change version import

--- pysdds/__init__.py
+++ pysdds/__init__.py
@@ -3,3 +3,11 @@ __author__ = "Nikita Kuklev"
 from pysdds.readers import read  # noqa: F401
 from pysdds.writers import write  # noqa: F401
 from pysdds.structures import SDDSFile  # noqa: F401
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("package-name")
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = '[unknown]'
